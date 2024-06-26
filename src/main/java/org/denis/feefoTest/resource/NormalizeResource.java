6627564d673e00a7d9addaaa793b09fff97be75f
denis <denis.vespasiano@live.com>
2024-06-25 23:22:36 -0300

    cleaning imports

--- src/main/java/org/denis/feefoTest/resource/NormalizeResource.java
+++ src/main/java/org/denis/feefoTest/resource/NormalizeResource.java
@@ -1,7 +1,6 @@
 package org.denis.feefoTest.resource;
 
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
